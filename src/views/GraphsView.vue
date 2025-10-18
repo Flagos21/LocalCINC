7684c6c574020498e3b738935d0f57de3ac1b8a1
Flagos21 <114185228+Flagos21@users.noreply.github.com>
2025-10-18 20:33:59 -0300

    Revamp magnetometer chart UI and defaults

--- src/views/GraphsView.vue
+++ src/views/GraphsView.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="max-w-7xl mx-auto">
-    <h1 class="text-2xl font-semibold mb-4">Magnetómetro – Estación única</h1>
+  <div class="py-6">
     <MagnetometerChartFigure />
   </div>
 </template>
